a7be700d2b4180d1cf0592b2cc4609430ee0a3ac
yanzur29 <144410867+yanzur29@users.noreply.github.com>
2023-11-28 08:54:28 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header ul li a');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top < offset + height) {
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header ul li a[href*=' + id + ']').classList.add('active');
+            });
+        };
+    });
+};
